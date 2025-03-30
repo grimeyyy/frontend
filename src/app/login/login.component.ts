15ecc21d5ad50bbfbc25392e5523a41d92c0df36
grimeyy <grimeyyyy@gmail.com>
2025-03-30 20:02:17 +0200

    reactive login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,24 +2,30 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Router, RouterModule} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  username = '';
-  password = '';
+
   errorMessage = '';
 
   constructor(private http: HttpClient, private router: Router) {}
 
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+
   login() {
-    this.http.post<{ token: string }>('/api/auth/login', { username: this.username, password: this.password })
+    this.http.post<{ token: string }>('/api/auth/login', this.loginForm.value)
       .subscribe({
         next: (response) => {
           localStorage.setItem('token', response.token);
