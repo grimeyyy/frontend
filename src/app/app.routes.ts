a40492bd6fed6f5b635a64e6627cbf4185e32fff
grimeyy <grimeyyyy@gmail.com>
2025-04-01 09:52:19 +0200

    some new components, routable modals, translations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {AuthGuard} from './guards/auth.guard';
+import {ModalWrapperComponent} from './modal-wrapper/modal-wrapper.component';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
-  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [AuthGuard] },
-  { path: '**', redirectTo: 'login' } // 404-Fallback
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent), children: [
+      { path: 'login', component: ModalWrapperComponent, data: {component: LoginComponent} },
+      { path: 'sign-up', component: ModalWrapperComponent, data: {component: SignUpComponent}},
+    ]
+  },
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [AuthGuard]
+  },
+  {path: '**', redirectTo: 'home'} // 404-Fallback
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+/// <reference types="@angular/localize" />
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {TranslateService} from '@ngx-translate/core';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(appRef => {
+    const translate = appRef.injector.get(TranslateService);
+    translate.setDefaultLang('en');
+    translate.use('en');
+  })
   .catch((err) => console.error(err));
