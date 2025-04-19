9d3288dd0a1728c8541630da6e560eb307fdb25a
grimeyyy <andres.lobacovs@proton.me>
2025-04-19 13:37:08 +0200

    setting up playwright e2e tests for frontend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /// <reference types="@angular/localize" />
 
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {appConfig} from './app/app.config';
+import {AppComponent} from './app/app.component';
 import {TranslateService} from '@ngx-translate/core';
 import {ThemeService} from './app/shared/services/theme.service';
 
